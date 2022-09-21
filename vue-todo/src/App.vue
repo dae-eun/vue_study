5a109cb3b2966be64da703d1f852bead6163cd8f
dae-eun <dannynam@iocentre.biz>
2022-09-21 17:00:09 +0900

    0920

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput></TodoInput>
-    <TodoList></TodoList>
+    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem"></TodoList>
     <TodoFooter></TodoFooter>
 
   </div>
@@ -15,16 +15,57 @@ import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
 export default {
+  data:function(){
+    return{
+      todoItems:[]
+    }
+  },
+  methods:{
+    addOneItem:function(todoItem){
+        var obj={completed:false, item: todoItem};
+        // localStorage.setItem(this.newTodoItem, obj);
+        localStorage.setItem(todoItem, JSON.stringify(obj));
+        this.todoItems.push(obj);
+    },
+    removeOneItem:function(todoItem, index){
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index,1);
+
+    }
+  },
+  created: function(){
+    if (localStorage.length>0){
+      for(var i=0; i<localStorage.length; i++){
+        // console.log(localStorage.key(i));
+        if(localStorage.key(i)!=='loglevel:webpack-dev-server'){
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i)))); 
+        }
+      }
+    }
+  },
  components:{
     //  컴포넌트 태그명 : 컴포넌트 내용
     'TodoHeader' : TodoHeader,
     'TodoInput' : TodoInput,
     'TodoList' : TodoList,
     'TodoFooter' : TodoFooter
- }
+ },
 }
 </script>
 
 <style>
-
+body{
+  text-align:center;
+  background-color: #f6f6f6;
+}
+input{
+  border-style:groove;
+  width:200px;
+}
+button{
+  border-style:groove;
+}
+.shadow{
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
 </style>
--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,16 +1,60 @@
 <template>
   <!--html-->
-  <div>
-    input
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+    <!-- <button v-on:click="addTodo">add</button> -->
+    <span class="addContainer">
+      <i class="fa-solid fa-plus" v-on:click="addTodo"></i>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
   //  js script
+  data:function () {
+    return{
+      newTodoItem:"",
+    }
+  },
+  methods:{
+    addTodo:function(){
+      if (this.newTodoItem!=='') {
+        this.$emit('addTodoItem', this.newTodoItem);
+        this.clearInput();
+      }
+    },
+    clearInput:function(){
+      this.newTodoItem='';
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
   /* css */
+  input:focus{
+    outline: none;
+  }
+  .inputBox{
+    background:#fff;
+    height:50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input{
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer{
+    float: right;
+    background: linear-gradient(to right, #6478fb, #8763fb);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn{
+    color:#fff;
+    vertical-align: middle;
+  }
 </style>
